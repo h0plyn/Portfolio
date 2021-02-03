12ad40645bb27d2ca85364bfb40bd493b7d7b004
Ricky Rhodes <ricky@rickyrhodes.com>
2021-02-02 22:37:56 -0500

    refactor for mobile first

--- client/Home.js
+++ client/Home.js
@@ -1,5 +1,20 @@
 import React, { useState } from 'react'
+import styled from 'styled-components'
+
 import Info from './Info'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  background-color: #ecdccb;
+`
+
+const Icon = styled.a`
+  color: #1d403b;
+  text-decoration: none;
+  padding: 0.5rem;
+`
 
 export default function Home() {
   const [info, setInfo] = useState(false)
@@ -7,36 +22,41 @@ export default function Home() {
   return (
     <div className="main-text">
       <div className="container">
-        <Home />
-        <div className="flex">
-          <a
-            className="icon fab fa-github-square fa-6x"
+        <Info />
+        <Container>
+          <Icon
+            className="icon fab fa-github-square fa-2x"
             href="https://github.com/h0plyn"
             target="blank"
-          ></a>
-          <a
-            className="icon fab fa-linkedin fa-6x"
+          ></Icon>
+
+          <Icon
+            className="icon fab fa-linkedin fa-2x"
             href="https://www.linkedin.com/in/rickyrhodes/"
             target="blank"
-          ></a>
-          <a
-            className="icon fab fa-twitter-square fa-6x"
+          ></Icon>
+          <Icon
+            className="icon fab fa-twitter-square fa-2x"
             href="https://twitter.com/h0plyn"
             target="blank"
-          ></a>
-          <a
-            className="icon fab fa-instagram-square fa-6x"
+          ></Icon>
+          <Icon
+            className="icon fab fa-instagram-square fa-2x"
             alt="Instagram"
             href="https://www.instagram.com/rickyrhodes/"
             target="blank"
-          ></a>
-          <div
-            className="icon fas fa-info-circle fa-6x"
+          ></Icon>
+          <Icon
+            className="icon fas fa-info-circle fa-2x"
             alt="Information"
             onClick={() => setInfo(!info)}
-          ></div>
+          ></Icon>
+        </Container>
+        <div className="flex">
+          {info
+            ? "I'm baby lyft occupy tilde jean shorts godard. Hashtag plaid truffaut, you probably haven't heard of them cardigan poke vegan. Subway tile roof party you probably haven't heard of them viral, intelligentsia mustache next level semiotics cornhole tote bag gluten-free tumeric hashtag. Photo booth butcher tacos pickled trust fund.Dummy text? More like dummy thicc text, amirite?"
+            : ''}
         </div>
-        <div className="flex">{info ? <Info /> : ''}</div>
       </div>
     </div>
   )
--- client/Info.js
+++ client/Info.js
@@ -1,22 +1,49 @@
 import React from 'react'
+import styled from 'styled-components'
+import { device } from './device'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem 2rem;
+  justify-content: center;
+  background-color: #ecdccb;
+`
+
+const MainText = styled.h1`
+  font-size: 1.7rem;
+  line-height: 2.48rem;
+  color: #3b5d55;
+`
 
 export default function Info(props) {
   return (
-    <h1>
-      Hi, I'm{' '}
-      <span style={{ color: '#1d403b', fontStyle: 'italic' }}>
-        Ricky Rhodes
-      </span>
-      . A former{' '}
-      <span style={{ color: '#1d403b', fontStyle: 'italic' }}>
-        creative professional
-      </span>{' '}
-      turned{' '}
-      <span style={{ color: '#1d403b', fontStyle: 'italic' }}>
-        Fullstack Software Engineer
-      </span>
-      . I'm currently learning at Fullstack Academy. In the meantime, you can
-      check out my current work and info below.{' '}
-    </h1>
+    <Container>
+      <MainText>
+        Hi, I'm{' '}
+        <span style={{ color: '#1d403b', fontStyle: 'italic' }}>
+          Ricky Rhodes
+        </span>
+        .
+      </MainText>
+      <MainText>
+        A former{' '}
+        <span style={{ color: '#1d403b', fontStyle: 'italic' }}>
+          creative professional
+        </span>{' '}
+        turned{' '}
+        <span style={{ color: '#1d403b', fontStyle: 'italic' }}>
+          Fullstack Software Engineer
+        </span>
+        .
+      </MainText>
+      <MainText>
+        I'm currently learning at{' '}
+        <span style={{ color: '#1d403b', fontStyle: 'italic' }}>
+          Fullstack Academy
+        </span>
+        . In the meantime, you can check out my current work and info below.{' '}
+      </MainText>
+    </Container>
   )
 }
