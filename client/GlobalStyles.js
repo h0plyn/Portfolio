3a336ed19f5511aeecff29f142a0b083068af611
Ricky Rhodes <ricky@rickyrhodes.com>
2021-04-01 22:48:36 -0400

    position and make blob responsive

--- client/GlobalStyles.js
+++ client/GlobalStyles.js
@@ -14,18 +14,21 @@ body {
     font-weight: bold;
     scroll-behavior: smooth;
     font-size: 16px;
+    overflow-x: hidden;
 }
 
 .blob {
     position: absolute;
     width: 120rem;
-    overflow: hidden;
-    left: 50%;
-    top: -50%;
-    height: 120rem;
-    opacity: 0.1;
+    top: -10rem;
     z-index: -1;
-    color: #abd1c6;
+    opacity: 0.1;
+}
+
+.overflow {
+    position: relative;
+    max-width: 100vw;
+    max-height: 100vh;
 }
 
 a {
@@ -43,31 +46,11 @@ a:hover {
     font-size: 4rem;
 }
 
-.custom-shape-divider-top-1612623275 {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    overflow: hidden;
-    line-height: 0;
-}
-
-.custom-shape-divider-top-1612623275 svg {
-    position: relative;
-    display: block;
-    width: calc(154% + 1.3px);
-    height: 52px;
-}
-
-.custom-shape-divider-top-1612623275 .shape-fill {
-    fill: #abd1c6;
-}
-
 /** For tablet devices **/
 @media (min-width: 768px) and (max-width: 1023px) {
-    .custom-shape-divider-top-1612623275 svg {
-        width: calc(154% + 1.3px);
-        height: 60px;
+    .blob {
+        width: 40rem;
+        margin-top: 5rem;
     }
 
     .icon {
@@ -77,9 +60,10 @@ a:hover {
 
 /** For mobile devices **/
 @media (max-width: 767px) {
-    .custom-shape-divider-top-1612623275 svg {
-        width: calc(154% + 1.3px);
-        height: 28px;
+    .blob {
+        width: 22rem;
+        margin-top: 15rem;
+        margin-right: 1rem;
     }
 
     .icon {
