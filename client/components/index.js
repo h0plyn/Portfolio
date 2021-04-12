a881e9d55025e8f632d8c18cb24d5d9a7adc2898
Ricky Rhodes <ricky@rickyrhodes.com>
2021-04-11 21:52:01 -0400

    break out app from root for readability

---
+++ client/components/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from './Header';
+import Projects from './Projects';
+import Shape from './Shape';
+import History from './History';
+import LinkBar from './LinkBar';
+import Footer from './Footer';
+
+export default function Main() {
+  return (
+    <>
+      <Shape />
+      <LinkBar />
+      <Header />
+      <Projects />
+      <History />
+      <Footer />
+    </>
+  );
+}
--- client/index.js
+++ client/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import GlobalStyles from './components/GlobalStyles';
-import Header from './components/Header';
-import Projects from './components/Projects';
-import currentProjects from './components/projectData';
-import Shape from './components/Shape';
-import History from './components/History';
-import LinkBar from './components/LinkBar';
-import Footer from './components/Footer';
+import GlobalStyles from './GlobalStyles';
+import Main from './components';
 import { motion } from 'framer-motion';
 import { ProjectProvider, projects } from './context/ProjectContext';
 import { HistoryProvider, history } from './context/HistoryContext';
@@ -27,12 +21,7 @@ function App() {
               duration: 0.6,
             }}
           >
-            <Shape />
-            <LinkBar />
-            <Header />
-            <Projects data={currentProjects} />
-            <History data={currentProjects} />
-            <Footer />
+            <Main />
           </motion.div>
         </HistoryProvider>
       </ProjectProvider>
