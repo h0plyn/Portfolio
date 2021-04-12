838c27b067aac91dddac03a8df55756feee84e44
Ricky Rhodes <ricky@rickyrhodes.com>
2021-04-11 20:08:02 -0400

    add animate y-axis on page load

--- client/index.js
+++ client/index.js
@@ -17,8 +17,8 @@ function App() {
       <GlobalStyles />
       <motion.div
         layout
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
         transition={{
           duration: 0.6,
         }}
